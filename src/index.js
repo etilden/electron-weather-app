d443b45740ed6e0b82d22e78fd0510b69c15dd3a
Emma Tilden <e.k.tilden@gmail.com>
2020-04-02 14:51:47 -0500

    router implemented

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-// import * as serviceWorker from './serviceWorker';
-// import WeatherHome from './components/WeatherHome'
+import { HashRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux'
+import store from "./store"
 
 ReactDOM.render(
-    <App />,
+  <Provider store = {store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 
